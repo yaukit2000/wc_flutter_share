051851881ce0ed5b9ea909f48dd195a4ea5a93ee
yaukit2000 <yaukit2000@hotmail.com>
2020-04-17 20:29:48 +0800

    fix non-optional type NSURL

--- ios/Classes/SwiftWcFlutterSharePlugin.swift
+++ ios/Classes/SwiftWcFlutterSharePlugin.swift
@@ -32,7 +32,7 @@ public class SwiftWcFlutterSharePlugin: NSObject, FlutterPlugin {
             let docsPath:String = NSSearchPathForDirectoriesInDomains(.cachesDirectory,.userDomainMask , true).first!
 //             let contentUri = NSURL(fileURLWithPath: docsPath).appendingPathComponent(fileName!)
             let contentUri = NSURL(fileURLWithPath: String(docsPath + fileName!))
-            contentToShare.append(contentUri!)
+            contentToShare.append(contentUri)
         }
         
         if (text != nil) {
